2b2727b43db8bfd4a8a18b866e96e560ccb6447e
riasingh125 <74634288+riasingh125@users.noreply.github.com>
2023-11-26 16:41:42 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,53 +1,38 @@
 import os
 import cv2
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import Bunch
+import imageio
 
-def resize_images(images, target_size=(64, 64)):
-    resized_images = []
-    for image in images:
-        resized_image = cv2.resize(image, target_size)
-        resized_images.append(resized_image)
-    return resized_images
+def resize_frame(frame, target_size=(64, 64)):
+    return cv2.resize(frame, target_size)
 
 def load_data(data_path):
-    images = []
+    df = pd.read_excel(data_path, header=None, names=['video', 'word'])
+    videos = []
     labels = []
 
-    for filename in os.listdir(data_path):
-        if filename.endswith(".mp4"):
-            video_path = os.path.join(data_path, filename)
-            cap = cv2.VideoCapture(video_path)
+    for index, row in df.iterrows():
+        video_path = row['video']
+        label = row['word']
 
-            # Capture the first frame
-            ret, frame = cap.read()
-            if not ret:
-                continue
+        # Read the video using imageio
+        video_reader = imageio.get_reader(video_path, 'ffmpeg')
+        frames = [resize_frame(frame, target_size=(64, 64)) for frame in video_reader]
 
-            # Resize the frame
-            resized_frame = cv2.resize(frame, (64, 64))
+        videos.append(frames)
+        labels.append(label)
 
-            # Append the resized frame to the images list
-            images.append(resized_frame)
-
-            # Extract the label from the filename (assuming filename format: label_word.mp4)
-            label = filename.split("_")[0]
-            labels.append(label)
-
-            cap.release()
-
-    return images, labels
+    return videos, labels
 
 def preprocess_data(data_path):
     # Load data
-    images, labels = load_data(data_path)
-
-    # Resize images
-    resized_images = resize_images(images)
+    videos, labels = load_data(data_path)
 
     # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(resized_images, labels, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(videos, labels, test_size=0.2, random_state=42)
 
     # Encode labels
     label_encoder = LabelEncoder()
@@ -65,6 +50,5 @@ def preprocess_data(data_path):
 
     return data
 
-
-data_path = "DataCSV.csv"
+data_path = "DataCSV.xlsx"  # Update with the path to your Excel dataset file
 preprocessed_data = preprocess_data(data_path)
